0908cccebacc464ec646e0fbbc8bb924c25f7ed2
Isak <it222hp@student.lnu.se>
2025-11-19 14:48:43 +0100

    Update test table

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -11,11 +11,11 @@
 // import { Password } from '../src/BugWrongMessage'
  // import { Password } from '../src/BugDoesNotHash'
  // import { Password } from '../src/BugDoesNotTrim'
- import { Password } from '../src/BugisPasswordAlwaysSame'
+ // import { Password } from '../src/BugisPasswordAlwaysSame'
  // import { Password } from '../src/BugMissingNumberCheck'
  // import { Password } from '../src/BugMissingPasswordCheck'
  // import { Password } from '../src/BugToShortPassword'
- //import { Password } from '../src/BugVeryShort'
+ import { Password } from '../src/BugVeryShort'
 
 describe('Password class, test suite', () => {
     //put constants here to increase readability
