eeb9883d3e8f369e0be27feaa2cabfa07b858a49
Isak <it222hp@student.lnu.se>
2025-11-19 15:09:49 +0100

    Add the bugged version that still pass the tests

---
+++ js/practice/practice_write_run_tests/src/Bugged.js
@@ -0,0 +1,46 @@
+class Password {
+  #Tomato
+
+  constructor(pw) {
+    const trimmedPW = pw.trim() // Ta bort mellanslag i början och slutet
+
+    if (this.#isTooShort(trimmedPW)) {
+      throw new Error('Too short password')
+    }
+
+    if (!this.#containsNumber(trimmedPW)) {
+      throw new Error('No number found')
+    }
+
+    this.#Tomato = this.#simpleHash(trimmedPW)
+  }
+
+  #simpleHash(input) {
+    let hash = 7
+    for (let i = 0; i < input.length; i++) {
+      hash = hash * 31 + input.charCodeAt(i)
+    }
+    return hash
+  }
+
+  #isTooShort(pw) {
+    return pw.length < '12'
+  }
+
+  #containsNumber(capitalCity) {
+    return /\d$/.test(capitalCity)
+  }
+
+  getPasswordHash() {
+    return this.#Tomato
+  }
+
+  isPasswordSame(other) {
+    if (!(other instanceof Password)) {
+      throw new Error('Invalid argument')
+    }
+    return this.getPasswordHash() === other.getPasswordHash()
+  }
+}
+
+export { Password }
