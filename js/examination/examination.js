773bfb5b6b9463c50a1ce712083fd95e26d9bbd1
Isak <it222hp@student.lnu.se>
2025-12-04 16:41:48 +0100

    Eximination passed

--- js/examination/examination.js
+++ js/examination/examination.js
@@ -1,1 +1,24 @@
 //You will be given a class that you need to test here. 
+class MusicTrack {
+    /**
+     * @param {String} title
+     * @param {String} artist
+     * @param {Number} duration
+     */
+    constructor(title, artist, duration) {
+        if (title.length === 0) {
+            throw new Error('Title cannot be empty');
+        }
+        if (artist.length === 0) {
+            throw new Error('artist cannot be empty');
+        }
+        if (duration <= 0) {
+            throw new Error('duration cannot be less than or equal to zero');
+        }
+        this.title = title;
+        this.artist = artist;
+        this.duration = duration; // duration in seconds
+    }
+}
+
+export { MusicTrack }
--- js/examination/examination.test.js
+++ js/examination/examination.test.js
@@ -1,7 +1,7 @@
 //You are going to write tests for the examination.js 
 
- // import { Password } from "./examination.js"; // Uncomment when Password class is added
- //import { expect, jest } from '@jest/globals';
+  import { MusicTrack } from "./examination.js"; // Uncomment when Password class is added
+ import { expect, jest } from '@jest/globals';
 
 
 
@@ -14,6 +14,19 @@ describe('Examination tests', () => {
     test('placeholder', () => {
         expect(false).toBe(false);
     });
+
+    test('constructor should throw an error if title length is 0', () => {
+
+
+        const title = ''
+        const artist = 'hej'
+        const duration = 10
+
+        expect(() => {
+           new MusicTrack(title, artist, duration)
+        }).toThrow('Title cannot be empty')
+
+    })
     
-    
+
 });
